f0a0258c6d62fa1e637b81592dfb61d5e376f2a5
Gabriel Felipe <ogabriel.v.felipe@gmail.com>
2026-02-11 10:44:59 -0300

    test: update FindAllUsersUseCase tests to reflect new dependencies and `execute` method signature

--- api/tests/unit/user/use-case/FindAllUsersUseCase.spec.ts
+++ api/tests/unit/user/use-case/FindAllUsersUseCase.spec.ts
@@ -2,18 +2,26 @@ import { FindAllUsersUseCase } from "@/core/application/user/use-case/FindAllUse
 import { User, UserRole } from "@/core/domain/user/entities/User";
 import { Email } from "@/core/domain/user/value-objects/Email";
 import { InMemoryUserRepository } from "../repositories/InMemoryUserRepository";
+import { CaslAbilityFactory } from "@/core/application/security/casl.factory";
+
+const loggerMock = {
+  warn: vi.fn(),
+  error: vi.fn(),
+  info: vi.fn(),
+};
 
 describe("FindAllUsersUseCase", () => {
   const repo = new InMemoryUserRepository();
 
   let useCase: FindAllUsersUseCase;
+  const factory = new CaslAbilityFactory();
 
   beforeEach(() => {
     vi.clearAllMocks();
 
     repo.clear();
 
-    useCase = new FindAllUsersUseCase(repo);
+    useCase = new FindAllUsersUseCase(repo, factory, loggerMock);
   });
 
   it("should be able to find all users", async () => {
@@ -27,7 +35,10 @@ describe("FindAllUsersUseCase", () => {
 
     repo.save(fakeUser);
 
-    const result = await useCase.execute({ filters: {}, limit: 10, page: 1 });
+    const result = await useCase.execute(
+      { filters: {}, limit: 10, page: 1 },
+      fakeUser.getId(),
+    );
 
     expect(result.docs).toHaveLength(1);
     expect(result.docs[0]).toBe(fakeUser);
@@ -55,11 +66,14 @@ describe("FindAllUsersUseCase", () => {
     repo.save(fakeUser);
     repo.save(fakeUser2);
 
-    const result = await useCase.execute({
-      filters: { username: "john.doe" },
-      limit: 10,
-      page: 1,
-    });
+    const result = await useCase.execute(
+      {
+        filters: { username: "john.doe" },
+        limit: 10,
+        page: 1,
+      },
+      fakeUser.getId(),
+    );
 
     expect(result.docs).toHaveLength(1);
     expect(result.docs[0].getUsername()).toBe(fakeUser.getUsername());
@@ -86,14 +100,20 @@ describe("FindAllUsersUseCase", () => {
     repo.save(fakeUser);
     repo.save(fakeUser2);
 
-    const result = await useCase.execute({ filters: {}, limit: 1, page: 1 });
+    const result = await useCase.execute(
+      { filters: {}, limit: 1, page: 1 },
+      fakeUser.getId(),
+    );
 
     expect(result.docs).toHaveLength(1);
     expect(result.docs[0]).toBe(fakeUser);
 
     expect(result.docs).toEqual(expect.arrayContaining([fakeUser]));
 
-    const result2 = await useCase.execute({ filters: {}, limit: 1, page: 2 });
+    const result2 = await useCase.execute(
+      { filters: {}, limit: 1, page: 2 },
+      fakeUser.getId(),
+    );
 
     expect(result2.docs).toHaveLength(1);
     expect(result2.docs[0]).toBe(fakeUser2);
